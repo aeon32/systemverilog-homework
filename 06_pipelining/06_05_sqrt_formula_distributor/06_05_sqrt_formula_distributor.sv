3f0396ec6836fe3cf6540171d2f61f84c85f5de9
user <user@mail.com>
2026-06-01 21:14:40 +0300

    Implement sqrt formula distributor

--- 06_pipelining/06_05_sqrt_formula_distributor/06_05_sqrt_formula_distributor.sv
+++ 06_pipelining/06_05_sqrt_formula_distributor/06_05_sqrt_formula_distributor.sv
@@ -42,49 +42,116 @@ module sqrt_formula_distributor
     // Hint:
     // Instantiate sufficient number of "formula_1_impl_1_top", "formula_1_impl_2_top",
     // or "formula_2_top" modules to achieve desired performance.
-    localparam instances_num = 5;
     localparam module_latency = 50;
+    localparam instances_num = module_latency;
     
     localparam distributor_index_width = $clog2(instances_num);
-    logic [distributor_index_width - 1 : 0] distributor_index;
+    logic [distributor_index_width - 1 : 0] worker_index;
+    logic [distributor_index_width - 1 : 0] read_index;
+
     
-    wire arg_valids [0:instances_num - 1];;
+    wire  arg_valids[0:instances_num - 1];
+
+    logic  [31:0] a_reg [0:instances_num - 1];
+    logic  [31:0] b_reg [0:instances_num - 1];
+    logic  [31:0] c_reg [0:instances_num - 1];
+
+    wire  [31:0] a_wire [0:instances_num - 1];
+    wire  [31:0] b_wire [0:instances_num - 1];
+    wire  [31:0] c_wire [0:instances_num - 1];
+
+    wire  [31:0] res_wire [0:instances_num - 1];
+
+    wire  res_wire_vld [0:instances_num - 1];
+
+
    
     generate
         genvar i;
         for (i = 0; i < instances_num; i++)
         begin
-            formula_1_impl_1_top formula_module
-            (
-                .rst(rst),
-                .clk(clk),
-                .arg_vld(arg_valids[i])
-
-            );
-            
-
             if (formula == 1)
-            begin : if_formula_1
-        
-            end    
-        
+            begin
+                if (impl == 1)
+                begin
+                    formula_1_impl_1_top formula_module_1_1
+                    (
+                        .rst(rst),
+                        .clk(clk),
+                        .arg_vld(arg_valids[i]),
+                        .a(a_wire[i]),
+                        .b(b_wire[i]),
+                        .c(c_wire[i]),
+                        .res(res_wire[i]),
+                        .res_vld(res_wire_vld[i])
+                    );
+                end
+                else if (impl == 2)
+                begin
+                    formula_1_impl_2_top formula_module_1_2
+                    (
+                        .rst(rst),
+                        .clk(clk),
+                        .arg_vld(arg_valids[i]),
+                        .a(a_wire[i]),
+                        .b(b_wire[i]),
+                        .c(c_wire[i]),
+                        .res(res_wire[i]),
+                        .res_vld(res_wire_vld[i])
+                    );
+                    
+                end
+            end
+            else if (formula == 2)
+            begin
+                    formula_2_top formula_module_2
+                    (
+                        .rst(rst),
+                        .clk(clk),
+                        .arg_vld(arg_valids[i]),
+                        .a(a_wire[i]),
+                        .b(b_wire[i]),
+                        .c(c_wire[i]),
+                        .res(res_wire[i]),
+                        .res_vld(res_wire_vld[i])
+                    );                
+                
+            end
 
+
+
+            assign arg_valids[i] = arg_vld && (i == worker_index);
+
+            assign a_wire[i] = arg_valids[i] ? a : a_reg[i];
+            assign b_wire[i] = arg_valids[i] ? b : b_reg[i];
+            assign c_wire[i] = arg_valids[i] ? c : c_reg[i];
         end
 
-        
-
     endgenerate
+
+    assign res = res_wire[read_index];
+    assign res_vld = res_wire_vld[read_index];
     
-
-
 
     always_ff @ (posedge clk)
     if (rst)
-        distributor_index <= '0;
+    begin
+        worker_index <= 0;
+        read_index <= 0;
+    end
     else begin
         if (arg_vld)
         begin
-            distributor_index<= distributor_index == instances_num - 1 ? 0 : distributor_index + 1;
+            a_reg[worker_index] <= a;
+            b_reg[worker_index] <= b;
+            c_reg[worker_index] <= c;
+
+            worker_index<= worker_index == instances_num - 1 ? 0 : worker_index + 1;
+        end
+
+        if (res_vld)
+        begin
+            read_index<= read_index == instances_num - 1 ? 0 : read_index + 1;
         end
     end
 
